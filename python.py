3c61eae047034eecc2f2babd6cbe4cd7a6989300
kgoodman <kellangoodman@gmail.com>
2022-08-31 19:36:10 -0700

    orloopbasic

---
+++ python.py
@@ -0,0 +1,30 @@
+for i in range(0, 151):
+    print(i)
+
+for i in range(5, 1001, 5):
+    print(i)
+
+drawers = ['coding', "coding dojo"]
+for v in range(0,101):
+    if v%10==0:
+        print(drawers[1])
+    elif v%5==0:
+        print(drawers[0])
+    else:
+        print(v)
+
+sum = 0
+for i in range(1, 500000, 2):
+    sum = sum + i
+print(sum)
+
+for i in range(2018, 0, -4):
+    print(i)
+
+for i in range(10, 101):
+    if i % 10 == 0:
+        print(i)
+
+
+
+
